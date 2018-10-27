64d8709fbdc01bf99e8fc07405742f9c063260a5
ItsMeMalario <Malario@asdf.pl>
2018-10-27 16:38:22 +0200

    zrobiono team.py

---
+++ okienka.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+window.title("TYTUL")
+window.geometry('800x600')
+
+lbl = Label(window, text = "podaj kod do systemu nasa...")
+lbl.grid(row = 0, column = 0)
+
+window.mainloop()
--- team.py
+++ team.py
@@ -1,16 +1,21 @@
-osoby = ["Mariusz","Stojan","Abecadel","Pawel","Ktos"]
+osoby = ["Mariusz","Stojan","Abecadel","Pawel","Ktos","Pimpek","Bombek"]
 team1 = []
 team2 = []
 import random
 
 try:
-    for x in range(0,len(osoby)):
+    powt = int(len(osoby)/2)
+    if powt < (len(osoby)/2):
+        powt+=1
+    
+    for x in range(0,powt):
         ran = random.choice(osoby)
         team1.append(ran)
         osoby.remove(ran)
-        ran = random.choice(osoby)
-        team2.append(ran)
-        osoby.remove(ran)
+        if len(osoby) > 0:
+            ran = random.choice(osoby)
+            team2.append(ran)
+            osoby.remove(ran)
     print(osoby,team1,team2)
 except ValueError as owcaError:
     print("blad")
